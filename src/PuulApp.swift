145b625ea3973f9f224581c69391714314dd0b8f
we-z <yousefsalem00@gmail.com>
2025-01-12 12:00:21 -0800

    LLamaState class and all llama.cpp code

--- src/PuulApp.swift
+++ src/PuulApp.swift
@@ -3,9 +3,6 @@ import SwiftUI
 
 @main
 struct PuulApp: App {
-    @StateObject public var appModel: AppModel = .init()
-    @Environment(\.scenePhase) var scenePhase
-
     var body: some Scene {
         WindowGroup {
             ZStack {
--- src/TempView.swift
+++ src/TempView.swift
@@ -1,8 +1,6 @@
-import Foundation
 import SwiftUI
 
 struct TempView: View {
-    @State private var inputText = ""
 
     var body: some View {
         Text("Hello, World!")
